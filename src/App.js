ba06f6fe9995285e324fcca37b8b0f9cbc385e29
Hatice-Altun <van.altun@outlook.com>
2023-12-30 22:54:43 +0100

    Netflix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  return (
+      <div className="App">
+          <Nav/>
+          <Banner/>
+      </div>
+  );
+}
+
+export default App;
